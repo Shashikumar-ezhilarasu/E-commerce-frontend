1bf681515ceaa73023b02fec05ead67f35fb4163
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2024-01-22 21:16:25 +0530

    shopify

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt="" />
-        <p>Shopping</p>
+        <p>Shopify</p>
       </div>
       <ul className="nav-menu">
         <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link> {menu==="shop"?<h/>:<></>}</li>
