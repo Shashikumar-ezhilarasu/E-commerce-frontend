e9b68da7a16732c5cc2747399e688af1ae9b6324
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2024-01-22 21:11:44 +0530

    added images of icons

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow_icon.png'
+import hero_image from '../Assets/hero_image.png'
 const Hero = () => {
   return (
     <div className='hero'>
@@ -21,7 +22,7 @@ const Hero = () => {
             </div>
         </div>
         <div className="hero-right">
-
+            <img src={hero_image} alt="" />
         </div>
     </div>
     
