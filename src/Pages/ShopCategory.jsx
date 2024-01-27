35b6b27f9dec98beead675e55f7af5a248682da6
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2024-01-28 00:01:30 +0530

    shopcategory perfectlayout

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,22 +1,38 @@
 import React, { useContext } from 'react';
 import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
 
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
 
+  // Filter products based on the category
+  const filteredProducts = all_product.filter(item => item.category === props.category);
+
   return (
     <div className='shop-category'>
       {/* Your component logic using the all_product context */}
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>Showing 1-{filteredProducts.length}</span> out of {all_product.length} products
         </p>
         <div className='shopcategory-sort'>
           Sort by <img src={dropdown_icon} alt="" />
         </div>
+      </div>
+      <div className="shopcategory-products">
+        {filteredProducts.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   );
